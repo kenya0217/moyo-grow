acfa0d88432b9f516d8369d9b1fc2fda8ac18418
Nakamura Kenya <pinpondream@yahoo.co.jp>
2018-08-24 23:54:31 +0900

    ん

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -170,7 +170,8 @@ public class KitchenSinkController {
 
     @Scheduled(initialDelay=0, fixedDelay=10000)
     public void execute(){
-        LineMessagingClient.pushMassage(new pushMassage(useid)
+        LineMessagingClient.pushMassage(new pushMassage(useid,new TemplateMessage("おは")).get();
+        log.info("fsa",response);
     }
 
     @EventMapping
